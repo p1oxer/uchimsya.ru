66ce0c3ac9d50b5e957e6d0c02d3a12f48a50171
p1oxer <pabonna@mail.ru>
2025-04-16 16:04:33 +0300

    1.1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Home() {
+    useEffect(() => {
+        const headerHeight = document.querySelector(".header").clientHeight;
+        headerHeight
+            ? (document.querySelector(".hero__body").style.paddingTop =
+                  headerHeight + "px")
+            : null;
+    }, []);
+
     return (
         <section className="hero">
             <div className="bg">
                 <img src="./img/bg.jpg" alt=""></img>
             </div>
             <div className="container">
-                123
+                <div className="hero__body">123</div>
             </div>
         </section>
     );
