ce9bbaf1c62c95c5cb267822867b1d8fb83a8993
p1oxer <pabonna@mail.ru>
2025-04-17 17:12:46 +0300

    1.2 hero swiper

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
 
-export default function Home() {
+const HeroSection = () => {
     useEffect(() => {
         const headerHeight = document.querySelector(".header").clientHeight;
         headerHeight
@@ -8,15 +12,36 @@ export default function Home() {
                   headerHeight + "px")
             : null;
     }, []);
-
     return (
         <section className="hero">
-            <div className="bg">
-                <img src="./img/bg.jpg" alt=""></img>
-            </div>
+            <Swiper
+                slidesPerView={1}
+                direction="horizontal"
+                loop={true}
+                speed={1000}
+                effect={"fade"}
+                className="hero__swiper"
+                modules={[EffectFade]}
+            >
+                <SwiperSlide>
+                    <div className="bg"></div>
+                    <div className="hero__bg">
+                        <img src="./img/bg.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="bg"></div>
+                    <div className="hero__bg">
+                        <img src="./img/bg2.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
             <div className="container">
                 <div className="hero__body">123</div>
             </div>
         </section>
     );
-}
+};
+
+export default HeroSection;
